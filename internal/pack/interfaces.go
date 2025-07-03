8cf2132d439a00491577908aad0a506d408cc7b1
Michael Salamakha <michaelsalamakha@gmail.com>
2025-07-03 04:41:58 +0200

    chore: typo

--- internal/pack/interfaces.go
+++ internal/pack/interfaces.go
@@ -2,7 +2,7 @@ package pack
 
 import "context"
 
-type PackRepository interface {
+type Repository interface {
 	GetPackSizes(ctx context.Context) ([]int, error)
 	InsertPackSize(ctx context.Context, size int) error
 	DeletePackSize(ctx context.Context, size int) error
